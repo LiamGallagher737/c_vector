71501832eb3eebfcc4bc92d5c7ad263d714162f8
Liam Gallagher <ljpgallagher@xtra.co.nz>
2023-08-29 17:25:15 +1200

    readme and demo comments

---
+++ README.md
@@ -0,0 +1,11 @@
+# Vector impl in C
+
+Haven't used C before so decided to try make a vector implementation for a quick and easy first project.
+
+## Demo
+
+A basic demo app showcasing some of the functionality of my implementation.
+
+```sh
+gcc vector.c demo.c -o demo ; ./demo
+```
--- demo.c
+++ demo.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+// Just to showcase something other than ints
 typedef struct Vec3
 {
     int x;
@@ -11,8 +12,10 @@ typedef struct Vec3
 
 int main(int argc, char *argv[])
 {
+    // Create a new vector with room for 10 elements
     Vector_t vec = with_capacity(10);
 
+    // Initialize with some values
     for (int n = 0; n < 10; n++)
     {
         Vec3_t *ptr = (Vec3_t *)malloc(sizeof(Vec3_t));
@@ -20,9 +23,11 @@ int main(int argc, char *argv[])
         append(&vec, ptr);
     }
 
+    // Remove an element
     Vec3_t *v = (Vec3_t *)take(&vec, 1);
     free(v);
 
+    // Add another 5 elements to showcase reallocating when length exceeds capacity
     for (int n = 0; n < 5; n++)
     {
         Vec3_t *ptr = (Vec3_t *)malloc(sizeof(Vec3_t));
@@ -30,6 +35,7 @@ int main(int argc, char *argv[])
         append(&vec, ptr);
     }
 
+    // Print values
     for (int n = 0; n < vec.length; n++)
     {
         Vec3_t *ptr = (Vec3_t *)get_index(&vec, n);
